7ccf989992a43e7bc3d30b683ce4be2717d691ea
Ryan" Yan Zhang <ryanz@mit.edu>
2016-07-06 17:47:51 -0400

    Virtual City Model Position Calibration UI - working

---
+++ Assets/RZ_script/CityModelTransform.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CityModelTransform : MonoBehaviour {
+	
+	public GameObject CityModel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CityModel.transform.position.Set ((float)CityModel.transform.position.x - 0.1f, (float)CityModel.transform.position.y, (float)CityModel.transform.position.z);
+	}
+
+	/*
+	public void xMinusDotOne () {
+		//float x = CityModel.transform.position.x;
+		CityModel.transform.position.Set ((float)CityModel.transform.position.x - 0.1f, (float)CityModel.transform.position.y, (float)CityModel.transform.position.z);
+	}
+
+	public void xMinusOne () {
+		//float x = CityModel.transform.position.x;
+		CityModel.transform.position.Set ((float)CityModel.transform.position.x - 1.0f, (float)CityModel.transform.position.y, (float)CityModel.transform.position.z);
+	}
+
+	public void xMinusTen () {
+		//float x = CityModel.transform.position.x;
+		CityModel.transform.position.Set ((float)CityModel.transform.position.x - 10.0f, (float)CityModel.transform.position.y, (float)CityModel.transform.position.z);
+	}
+
+	public void xMinusHundred () {
+		//float x = CityModel.transform.position.x;
+		CityModel.transform.position.Set ((float)CityModel.transform.position.x - 100.0f, (float)CityModel.transform.position.y, (float)CityModel.transform.position.z);
+	}
+	*/
+}
---
+++ Assets/RZ_script/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+	}
+
+	void FixedUpdate() //for physics
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count += 1;
+			SetCountText ();
+		}
+		
+		//Destroy(other.gameObject);
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+	}
+}
