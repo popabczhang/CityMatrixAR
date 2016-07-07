93d13c24489db7a3a8ce106cd1801cbb9c3d584f
Ryan" Yan Zhang <ryanz@mit.edu>
2016-07-07 16:42:29 -0400

    AR UI test
    meeting with Kent

---
+++ Assets/HighlightRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightRotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 15, 0) * Time.deltaTime);
+	}
+}
