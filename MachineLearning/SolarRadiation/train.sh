bd51c3e515022f9c2522f966b3e3304360be8121
A-lxe <alex@alxe.co>
2016-07-18 00:23:41 -0400

    Fixed train.sh to both work, and play nicely when called from another directory

--- MachineLearning/SolarRadiation/train.sh
+++ MachineLearning/SolarRadiation/train.sh
@@ -1,6 +1,10 @@
 # Script to extract training data and train a machine learning algorithm on that data, outputting the model information file to the given directory
 # Author: Alex "Alxe" Aubuchon
 
+# Change to the proper working directory
+PREV_DIR="$(pwd)"
+cd "$(dirname "${BASH_SOURCE}")"
+
 ZIP_DIR="./data"
 RAW_DIR="./data/raw"
 SCRIPTS_DIR="./scripts"
@@ -8,21 +12,25 @@ TRAIN_SCRIPT="$SCRIPTS_DIR/trainLinRegModel.py"
 OUTDIR="$1"
 OUTNAME="solar-linear.regr"
 
-if [[ "$OUTDIR" == "" ]]; then
-  echo "No outfile specified. Usage: ./train.sh [Regression Output Directory]"
+# Check if all arguments properly supplied
+if [[ "$OUTDIR" == "" || ! -d "$OUTDIR"  ]]; then
+  echo "No outdir specified. Usage: ./train.sh [Regression Output Directory]"
   exit 1
 fi
 
+# Extract data if there are no files in RAW_DIR
 if [[ !(-d "$RAW_DIR" && $(ls -A "$RAW_DIR")) ]]; then
   echo "No data files. Extracting from available .zip files..."
-  for file in "$(ls $ZIP_DIR)"; do
+  for file in $(ls -A $ZIP_DIR); do
     if [[ "${file: -4}" == ".zip" ]]; then
-    echo "$file"
-    unzip -jqo "$DATA_DIR/$file" -d "$RAW_DIR"
+      echo "$file"
+      unzip -jqo "$ZIP_DIR/$file" -d "$RAW_DIR"
     fi
   done
 fi
 
+# Run the training script
 python "$TRAIN_SCRIPT" "$RAW_DIR" "$OUTDIR/$OUTNAME"
 
+cd "$PREV_DIR"
 exit 0
