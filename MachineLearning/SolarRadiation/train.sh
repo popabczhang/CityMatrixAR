e962e24fe9d38f82ae4f531e4faf8e1caaf3dee6
A-lxe <alex@alxe.co>
2016-07-17 21:32:12 -0400

    A few more best-practice tweaks to train.sh

--- MachineLearning/SolarRadiation/train.sh
+++ MachineLearning/SolarRadiation/train.sh
@@ -5,24 +5,24 @@ ZIP_DIR="./data"
 RAW_DIR="./data/raw"
 SCRIPTS_DIR="./scripts"
 TRAIN_SCRIPT="$SCRIPTS_DIR/trainLinRegModel.py"
-OUTDIR=$1
+OUTDIR="$1"
 OUTNAME="solar-linear.regr"
 
-if [[ $OUTFILE == "" ]]; then
+if [[ "$OUTDIR" == "" ]]; then
   echo "No outfile specified. Usage: ./train.sh [Regression Output Directory]"
   exit 1
 fi
 
-if [[ !(-d $RAW_DIR && $(ls -A $RAW_DIR)) ]]; then
+if [[ !(-d "$RAW_DIR" && $(ls -A "$RAW_DIR")) ]]; then
   echo "No data files. Extracting from available .zip files..."
-  for file in $(ls $ZIP_DIR); do
-    if [[ ${file: -4} == ".zip" ]]; then
-    echo $file
-    unzip -jqo "$DATA_DIR/$file" -d $RAW_DIR
+  for file in "$(ls $ZIP_DIR)"; do
+    if [[ "${file: -4}" == ".zip" ]]; then
+    echo "$file"
+    unzip -jqo "$DATA_DIR/$file" -d "$RAW_DIR"
     fi
   done
 fi
 
-python $TRAIN_SCRIPT $RAW_DIR "$OUTDIR/$OUTNAME"
+python "$TRAIN_SCRIPT" "$RAW_DIR" "$OUTDIR/$OUTNAME"
 
 exit 0
