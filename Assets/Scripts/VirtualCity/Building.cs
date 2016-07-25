d8bdaa9044a04380bd7467ece4c7780f7667be83
A-lxe <alex@alxe.co>
2016-07-25 16:24:21 -0400

    Beginnings of a virtual city model using dynamic building prefabs.

---
+++ Assets/Scripts/VirtualCity/Building.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour
+{
+
+    public int height = 1;
+    public int vertex;
+
+    private MeshFilter mFilter;
+
+    // Use this for initialization
+    void Start()
+    {
+        mFilter = GetComponent<MeshFilter>();
+        initializeMesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int val = Random.Range(1, 5);
+        Debug.Log(val);
+        changeHeight(val);
+    }
+
+    void changeHeight(float h)
+    {
+        //Vertices where y != 0 from initializeMesh()
+        int[] positions = new int[]
+        {
+            1, 2, 4, 5, 6, 7, 9, 10, 17, 18, 21, 22
+        };
+        Vector3[] newVerts = mFilter.mesh.vertices;
+
+        foreach(int i in positions)
+        {
+            newVerts[i].y = h;
+        }
+
+        mFilter.mesh.vertices = newVerts;
+    }
+
+    void initializeMesh()
+    {
+        mFilter.mesh = new Mesh();
+        Vector3[] newVerts = new Vector3[] {
+            new Vector3(0, 0, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(1, 1, 0),
+            new Vector3(1, 0, 0),
+
+            new Vector3(0, 1, 0),
+            new Vector3(0, 1, 1),
+            new Vector3(1, 1, 1),
+            new Vector3(1, 1, 0),
+
+            new Vector3(1, 0, 0),
+            new Vector3(1, 1, 0),
+            new Vector3(1, 1, 1),
+            new Vector3(1, 0, 1),
+
+            new Vector3(0, 0, 1),
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(1, 0, 1),
+
+            new Vector3(0, 0, 1),
+            new Vector3(0, 1, 1),
+            new Vector3(0, 1, 0),
+            new Vector3(0, 0, 0),
+
+            new Vector3(1, 0, 1),
+            new Vector3(1, 1, 1),
+            new Vector3(0, 1, 1),
+            new Vector3(0, 0, 1),
+        };
+
+        Vector3[] normals = new Vector3[]
+        {
+            new Vector3(0, 0, -1),
+            new Vector3(0, 0, -1),
+            new Vector3(0, 0, -1),
+            new Vector3(0, 0, -1),
+
+            new Vector3(0, 1, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(0, 1, 0),
+
+            new Vector3(1, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(1, 0, 0),
+
+            new Vector3(0, -1, 0),
+            new Vector3(0, -1, 0),
+            new Vector3(0, -1, 0),
+            new Vector3(0, -1, 0),
+
+            new Vector3(-1, 0, 0),
+            new Vector3(-1, 0, 0),
+            new Vector3(-1, 0, 0),
+            new Vector3(-1, 0, 0),
+
+            new Vector3(0, 0, -1),
+            new Vector3(0, 0, -1),
+            new Vector3(0, 0, -1),
+            new Vector3(0, 0, -1)
+        };
+
+        int[] triangles = new int[]
+        {
+            0, 2, 3, 0, 1, 2,
+            4, 6, 7, 4, 5, 6,
+            8, 10, 11, 8, 9, 10,
+            12, 14, 15, 12, 13, 14,
+            16, 18, 19, 16, 17, 18,
+            20, 22, 23, 20, 21, 22
+
+        };
+
+        mFilter.mesh.vertices = newVerts;
+        mFilter.mesh.normals = normals;
+        mFilter.mesh.triangles = triangles;
+    }
+}
---
+++ Assets/Scripts/VirtualCity/VirtualCityModel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VirtualCityModel : MonoBehaviour {
+
+    public int buildingsX;
+    public int buildingsY;
+    public int buildingWidth;
+    public int buildingHeight;
+    public DataDisplay dataDisplay;
+
+    private SolarRadiation solarRadiation;
+
+    public enum DataDisplay {SolarRadiation};
+
+	// Use this for initialization
+	void Start () {
+        solarRadiation = GetComponent<SolarRadiation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
