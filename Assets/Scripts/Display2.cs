bd88ab507b044803d01dbcc914c5fd203e97e060
A-lxe <alex@alxe.co>
2016-10-17 22:21:57 -0400

    Added second display functionality

---
+++ Assets/Scripts/Display2.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Display2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        Display.displays[1].Activate();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
