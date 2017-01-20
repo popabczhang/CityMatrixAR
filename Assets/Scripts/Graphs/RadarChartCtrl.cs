e1068fc626fc72989853111e434ce4859cbf3e38
Alex Aubuchon <alex@alxe.co>
2017-01-19 21:54:35 -0500

    Added a (rough) utility for editing multiple radar charts at once. Cleaned up the scene.

---
+++ Assets/Scripts/Graphs/RadarChartCtrl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RadarChartCtrl : MonoBehaviour {
+
+	[Range(0, 1f)]
+	public float[] values;
+	float[] prev = new float[0];
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+			if (!values.SequenceEqual(prev)) {
+				prev = (float[]) values.Clone();
+				foreach(Transform c in this.transform) {
+					UnityEngine.UI.Extensions.RadarPolygon child = c.GetComponent<UnityEngine.UI.Extensions.RadarPolygon>();
+					UpdateChild(child);
+				}
+			}
+
+			values[3] = (float) (values[3] + 0.1) % 1;
+
+	}
+
+	void OnValidate () {
+		if (!values.SequenceEqual(prev)) {
+			prev = (float[]) values.Clone();
+			foreach(Transform c in this.transform) {
+				UnityEngine.UI.Extensions.RadarPolygon child = c.GetComponent<UnityEngine.UI.Extensions.RadarPolygon>();
+				UpdateChild(child);
+				child.Update();
+			}
+		}
+	}
+
+	void UpdateChild(UnityEngine.UI.Extensions.RadarPolygon child) {
+		child.value = (float[]) values.Clone();
+
+	}
+}
