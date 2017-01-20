41b2a8d65728fb37e716074e9d9cf78cfb0694c0
Alex Aubuchon <alex@alxe.co>
2017-01-20 00:05:14 -0500

    Added in basic camera switcher. Will need some more work on site.

--- Assets/Scripts/KinectMesh.cs
+++ Assets/Scripts/KinectMesh.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KinectMesh : MonoBehaviour {
-    
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButtonDown("KinectMesh"))
         {
-            Debug.Log("Disabling Kinect Mesh");
+            Debug.Log("Disabling/Enabling Kinect Mesh");
             foreach(Transform child in this.transform)
             {
                 Debug.Log(child);
